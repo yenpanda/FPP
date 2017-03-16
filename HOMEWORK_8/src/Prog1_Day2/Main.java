55532de295ff967e64d8877dddabf6cd43cbda9b
yenpanda <yentt1905@gmail.com>
2017-03-15 22:26:57 -0500

    Sumbit Assignment 8 Day 2

--- HOMEWORK_8/src/Prog1_Day2/Main.java
+++ HOMEWORK_8/src/Prog1_Day2/Main.java
@@ -1,3 +1,11 @@
+/* RESULT
+[Ha , Iphone , 20000.0, AAA , Sony , 1000.0, Yen , Iphone , 500.0]
+[Ha , Iphone , 20000.0, Yen , Iphone , 500.0]
+[Navi , Iphone , 3000.0, Yen , Iphone , 500.0]
+ArrayList size: 2
+Marketing Comparator: 1
+List of Sale more than 1000: [Navi , Iphone , 3000.0]
+ */
 package Prog1_Day2;
 
 import java.util.ArrayList;
@@ -7,25 +15,35 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayList<Marketing> ma = new ArrayList();
-		ma.add(new Marketing("Ha","Iphone",200));
+		ma.add(new Marketing("Ha","Iphone",20000));
+		ma.add(new Marketing("AAA","Sony",1000));
 		ma.add(new Marketing("Yen","Iphone",500));
 		System.out.println(ma);
 		ma.remove(1);
 		System.out.println(ma);
-		ma.set(0, new Marketing("Navi","Iphone",300));
+		ma.set(0, new Marketing("Navi","Iphone",3000));
+		System.out.println(ma);
 
 		Marketing x = new Marketing("AA", "Samsung", 1000);
 		Marketing y = new Marketing("BB", "Sony", 1300);
 		
-		System.out.println(ma.size());
+		System.out.println("ArrayList size: "+ ma.size());
 		
 		MarketingComparator maCom = new MarketingComparator();
+		System.out.println("Marketing Comparator: " + maCom.compare(x, y));
 		
-		
-		System.out.println(maCom.compare(x, y));
-		//Marketing[] a = new Marketing[1];
-		
+		ArrayList<Marketing> test = list1000(ma);
+		System.out.println("List of Sale more than 1000: " + test);
 
+	}
+	
+	public static ArrayList<Marketing> list1000(ArrayList<Marketing> list){
+		ArrayList<Marketing> tmp = new ArrayList<Marketing>();
+		for(Marketing a :list){
+			if(a.getSale() >= 1000)
+				tmp.add(a);
+		}
+		return tmp;
 	}
 
 }
--- HOMEWORK_8/src/Prog1_Day2/MarketingComparator.java
+++ HOMEWORK_8/src/Prog1_Day2/MarketingComparator.java
@@ -7,14 +7,15 @@ public class MarketingComparator implements Comparator<Marketing>{
 	@Override
 	public int compare(Marketing o1, Marketing o2) {
 		// TODO Auto-generated method stub
-		if(o1.getEmp().compareTo(o2.getEmp())!=0){
-			return o1.getEmp().compareTo(o2.getEmp());
-		}
-		else if(o1.getProduct().compareTo(o2.getProduct())!=0){
-			return o1.getProduct().compareTo(o2.getProduct());
-		}
-		else if(o1.getSale()!=o2.getSale()){
-			return (int)o1.getSale() - (int)o2.getSale();
+		if(o1.getSale() < o2.getSale())
+			return 1;
+		else if(o1.getSale() < o2.getSale())
+			return -1;
+		else if (o1.getSale() == o2.getSale()){
+			if(o1.getEmp().compareTo(o2.getEmp())!=0)
+				return o1.getEmp().compareTo(o2.getEmp());	
+			else 
+				return o1.getProduct().compareTo(o2.getProduct());
 		}
 		return 0;
 	}
